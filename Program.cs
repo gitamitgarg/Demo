26435b85f09422a2e2f3e7c5e6821d389ec7ba9b
gitamitgarg <amitgarg.mca@hotmail.com>
2020-11-16 21:44:42 +0530

    second commit

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ namespace gitconsoleapp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Git App is ready");
+            Console.ReadLine();
         }
     }
 }
