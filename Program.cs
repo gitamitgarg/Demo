2dad40b904f677091e64e5d77ab197c29856b249
gitamitgarg <amitgarg.mca@hotmail.com>
2020-11-16 21:17:44 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gitconsoleapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
